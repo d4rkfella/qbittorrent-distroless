6733c190a9860581b22df1b98bb6c3d53e2a2239
Georgi Panov <georgi.panov@darkfellanetwork.com>
2025-02-15 20:52:37 +0200

    Create qbittorrent-startup.go

---
+++ qbittorrent-startup.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	configFile := "/config/qBittorrent/qBittorrent.conf"
+	logFile := "/config/qBittorrent/logs/qbittorrent.log"
+
+	os.Setenv("QBT_WEBUI_PORT", os.Getenv("QBITTORRENT__PORT"))
+	os.Setenv("QBT_TORRENTING_PORT", os.Getenv("QBITTORRENT__BT_PORT"))
+
+	if _, err := os.Stat(configFile); os.IsNotExist(err) {
+		fmt.Println("Copying over default configuration ...")
+		err := os.MkdirAll(filepath.Dir(configFile), os.ModePerm)
+		if err != nil {
+			fmt.Printf("Error creating config directory: %v\n", err)
+			return
+		}
+		err = os.CopyFile("/app/qBittorrent.conf", configFile)
+		if err != nil {
+			fmt.Printf("Error copying config file: %v\n", err)
+			return
+		}
+	}
+
+	if _, err := os.Stat(logFile); os.IsNotExist(err) {
+		err := os.MkdirAll(filepath.Dir(logFile), os.ModePerm)
+		if err != nil {
+			fmt.Printf("Error creating log directory: %v\n", err)
+			return
+		}
+		err = os.Symlink("/proc/self/fd/1", logFile)
+		if err != nil {
+			fmt.Printf("Error creating symlink for log file: %v\n", err)
+			return
+		}
+	}
+
+	cmd := exec.Command("/app/qbittorrent-nox")
+	cmd.Args = append(cmd.Args, os.Args[1:]...) // Pass any command-line arguments
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Error running qBittorrent: %v\n", err)
+	}
+}
