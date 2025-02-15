59ff1476074e60ddba579fc15f8043443322ea6f
Georgi Panov <georgi.panov@darkfellanetwork.com>
2025-02-15 23:24:38 +0200

    Update cross-seed.go

--- cross-seed.go
+++ cross-seed.go
@@ -6,98 +6,183 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
-var (
-	CrossSeedEnabled        = getEnv("CROSS_SEED_ENABLED", "false")
-	CrossSeedURL            = getEnv("CROSS_SEED_URL", "required")
-	CrossSeedAPIKey         = getEnv("CROSS_SEED_API_KEY", "required")
-	CrossSeedSleepInterval  = getEnv("CROSS_SEED_SLEEP_INTERVAL", "30")
-	PushoverEnabled         = getEnv("PUSHOVER_ENABLED", "false")
-	PushoverUserKey         = getEnv("PUSHOVER_USER_KEY", "required")
-	PushoverToken           = getEnv("PUSHOVER_TOKEN", "required")
-)
+// Config holds the configuration variables
+type Config struct {
+	CrossSeedEnabled      bool
+	CrossSeedURL          string
+	CrossSeedAPIKey       string
+	CrossSeedSleepInterval int
+	PushoverEnabled       bool
+	PushoverUserKey       string
+	PushoverToken         string
+}
+
+// ReleaseInfo holds the release information from qBittorrent
+type ReleaseInfo struct {
+	Name     string
+	InfoHash string
+	Category string
+	Size     int64
+	Indexer  string
+	Type     string
+}
+
+var config Config
+
+func init() {
+	// Initialize configuration from environment variables
+	config.CrossSeedEnabled = getEnvBool("CROSS_SEED_ENABLED", false)
+	config.CrossSeedURL = getEnv("CROSS_SEED_URL", "required")
+	config.CrossSeedAPIKey = getEnv("CROSS_SEED_API_KEY", "required")
+	config.CrossSeedSleepInterval = getEnvInt("CROSS_SEED_SLEEP_INTERVAL", 30)
+	config.PushoverEnabled = getEnvBool("PUSHOVER_ENABLED", false)
+	config.PushoverUserKey = getEnv("PUSHOVER_USER_KEY", "required")
+	config.PushoverToken = getEnv("PUSHOVER_TOKEN", "required")
+}
+
+func main() {
+	if len(os.Args) != 6 {
+		fmt.Fprintln(os.Stderr, "Usage: <program> <release_name> <info_hash> <category> <size> <indexer>")
+		os.Exit(1)
+	}
+
+	release := ReleaseInfo{
+		Name:     os.Args[1],
+		InfoHash: os.Args[2],
+		Category: os.Args[3],
+		Size:     parseSize(os.Args[4]),
+		Indexer:  os.Args[5],
+		Type:     "Torrent",
+	}
+
+	if config.PushoverEnabled {
+		if err := sendPushoverNotification(release); err != nil {
+			fmt.Fprintf(os.Stderr, "Error sending Pushover notification: %v\n", err)
+		}
+	}
+
+	if config.CrossSeedEnabled {
+		if err := searchCrossSeed(release); err != nil {
+			fmt.Fprintf(os.Stderr, "Error searching CrossSeed: %v\n", err)
+		}
+	}
+}
 
 func getEnv(key, defaultValue string) string {
-	value := os.Getenv(key)
-	if value == "" {
+	value, exists := os.LookupEnv(key)
+	if !exists {
 		return defaultValue
 	}
 	return value
 }
 
-func setQbVars(args []string) (string, string, string, string, string) {
-	return args[0], args[1], args[2], args[3], args[4]
+func getEnvBool(key string, defaultValue bool) bool {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value == "true"
 }
 
-func sendPushoverNotification(releaseName, releaseCat, releaseIndexer, releaseSize string) {
-	pushoverMessage := fmt.Sprintf("<b>%s</b><small>\n<b>Category:</b> %s</small><small>\n<b>Indexer:</b> %s</small><small>\n<b>Size:</b> %s</small>",
-		releaseName, releaseCat, releaseIndexer, releaseSize)
+func getEnvInt(key string, defaultValue int) int {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	intValue, err := strconv.Atoi(value)
+	if err != nil {
+		return defaultValue
+	}
+	return intValue
+}
 
-	jsonData := map[string]interface{}{
-		"token":   PushoverToken,
-		"user":    PushoverUserKey,
-		"title":   "Torrent Downloaded",
-		"message": pushoverMessage,
-		"priority": -2,
-		"html":     1,
+func parseSize(sizeStr string) int64 {
+	size, err := strconv.ParseInt(sizeStr, 10, 64)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing size: %v\n", err)
+		os.Exit(1)
+	}
+	return size
+}
+
+func sendPushoverNotification(release ReleaseInfo) error {
+	u, err := url.Parse(release.Indexer)
+	if err != nil {
+		return fmt.Errorf("error parsing indexer URL: %v", err)
 	}
 
-	jsonBody, err := json.Marshal(jsonData)
-	if err != nil {
-		fmt.Printf("Error marshalling JSON: %v\n", err)
-		return
+	message := fmt.Sprintf("<b>%s</b><small>\n<b>Category:</b> %s</small><small>\n<b>Indexer:</b> %s</small><small>\n<b>Size:</b> %s</small>",
+		strings.TrimSuffix(release.Name, ".torrent"),
+		release.Category,
+		u.Hostname(),
+		formatSize(release.Size),
+	)
+
+	data := map[string]string{
+		"token":     config.PushoverToken,
+		"user":      config.PushoverUserKey,
+		"title":     fmt.Sprintf("%s Downloaded", release.Type),
+		"message":   message,
+		"priority":  "-2",
+		"html":      "1",
 	}
 
-	resp, err := http.Post("https://api.pushover.net/1/messages.json", "application/json", bytes.NewBuffer(jsonBody))
+	jsonData, err := json.Marshal(data)
 	if err != nil {
-		fmt.Printf("Error sending Pushover notification: %v\n", err)
-		return
+		return fmt.Errorf("error marshalling JSON: %v", err)
+	}
+
+	resp, err := http.Post("https://api.pushover.net/1/messages.json", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return fmt.Errorf("error sending Pushover notification: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Printf("Pushover notification response: %v\n", string(body))
+	fmt.Printf("Pushover notification returned with HTTP status code %d and payload: %s\n", resp.StatusCode, string(body))
+	return nil
 }
 
-func searchCrossSeed(releaseInfoHash string) {
-	client := &http.Client{}
-	data := fmt.Sprintf("infoHash=%s&includeSingleEpisodes=true", releaseInfoHash)
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/webhook", CrossSeedURL), strings.NewReader(data))
-	if err != nil {
-		fmt.Printf("Error creating request: %v\n", err)
-		return
-	}
-	req.Header.Add("X-Api-Key", CrossSeedAPIKey)
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+func searchCrossSeed(release ReleaseInfo) error {
+	data := url.Values{}
+	data.Set("infoHash", release.InfoHash)
+	data.Set("includeSingleEpisodes", "true")
 
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/webhook", config.CrossSeedURL), strings.NewReader(data.Encode()))
+	if err != nil {
+		return fmt.Errorf("error creating CrossSeed request: %v", err)
+	}
+	req.Header.Set("X-Api-Key", config.CrossSeedAPIKey)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("Error sending cross-seed search request: %v\n", err)
-		return
+		return fmt.Errorf("error sending CrossSeed request: %v", err)
 	}
 	defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Printf("Cross-seed search response: %v\n", string(body))
-
-	sleepInterval, _ := time.ParseDuration(CrossSeedSleepInterval + "s")
-	time.Sleep(sleepInterval)
+	fmt.Printf("Cross-seed search returned with HTTP status code %d and path %s\n", resp.StatusCode, release.Name)
+	time.Sleep(time.Duration(config.CrossSeedSleepInterval) * time.Second)
+	return nil
 }
 
-func main() {
-	args := os.Args[1:]
-
-	releaseName, releaseInfoHash, releaseCat, releaseSize, releaseIndexer := setQbVars(args)
-
-	if PushoverEnabled == "true" {
-		sendPushoverNotification(releaseName, releaseCat, releaseIndexer, releaseSize)
+func formatSize(size int64) string {
+	const unit = 1024
+	if size < unit {
+		return fmt.Sprintf("%d B", size)
 	}
-
-	if CrossSeedEnabled == "true" {
-		searchCrossSeed(releaseInfoHash)
+	div, exp := int64(unit), 0
+	for n := size / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
 	}
+	return fmt.Sprintf("%.2f %ciB", float64(size)/float64(div), "KMGTPE"[exp])
 }
